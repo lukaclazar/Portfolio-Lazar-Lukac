498025a6bc486a9d33576e296872dea27e1f7688
lukaclazar <lazarlukac2004@gmail.com>
2026-05-06 23:23:09 -0700

    final final (i hope)

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// LIGHT / DARK MODE
+const toggleBtn = document.getElementById("theme-toggle");
 
-const toggle = document.getElementById("theme-toggle");
-
-toggle.addEventListener("click", () => {
+toggleBtn.addEventListener("click", function(){
 
 document.body.classList.toggle("light-mode");
 
 if(document.body.classList.contains("light-mode")){
-toggle.textContent = "Dark Mode";
+toggleBtn.textContent = "Dark Mode";
 }
 else{
-toggle.textContent = "Light Mode";
+toggleBtn.textContent = "Light Mode";
 }
 
 });
@@ -18,18 +16,60 @@ toggle.textContent = "Light Mode";
 
 
 
-// SIMPLE CONTACT FORM
+
+
+const buttons = document.querySelectorAll(".more-btn");
+
+buttons.forEach(function(button){
+
+button.addEventListener("click", function(){
+
+const info = button.nextElementSibling;
+
+if(info.style.display === "block"){
+
+info.style.display = "none";
+button.textContent = "Show More";
+
+}
+else{
+
+info.style.display = "block";
+button.textContent = "Show Less";
+
+}
+
+});
+
+});
+
+
+
+
+
 
 const form = document.getElementById("contact-form");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function(e){
 
 e.preventDefault();
 
-const message = document.getElementById("form-message");
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const message = document.getElementById("message").value;
 
-message.textContent = "Message sent successfully!";
+const formMessage = document.getElementById("form-message");
 
+if(name === "" || email === "" || message === ""){
+
+formMessage.textContent = "Please fill in all fields.";
+
+}
+else{
+
+formMessage.textContent = "Message sent successfully!";
 form.reset();
+
+}
 
 });
